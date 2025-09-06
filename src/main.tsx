457111cd268698a0e6c9ed2478c4a0797ffa7d44
jvcbyte <jvc8463@gmail.com>
2025-09-06 16:05:03 +0100

    mv /index.css /styles/index.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import AppRoutes from './routes.tsx'
+import './styles/index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
