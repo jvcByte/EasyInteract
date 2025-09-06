30956d96e8e6d0febd00196009844efc1c80e2a9
jvcbyte <jvc8463@gmail.com>
2025-09-06 01:07:48 +0100

    basic input parameters

--- src/contract-interact/Interact.tsx
+++ src/contract-interact/Interact.tsx
@@ -1,10 +1,33 @@
+import Header from "../component/Header";
+
 export default function Interact() {
     return (
         <div>
-            <h1>Interact</h1>
-
-            <textarea name="data" id="">data</textarea>
-            <button>Interact</button>
+            <div>
+                <Header />
+            </div>
+            <div>
+                <h2 className="font-bold p-2 text-2xl">Interact</h2>
+                <div className="flex flex-col gap-2">
+                    <input
+                        type="text"
+                        placeholder="Enter RPC URL"
+                        className="w-full p-2 border border-gray-700 rounded-md"
+                    />
+                    <input
+                        type="text"
+                        placeholder="Enter contract address"
+                        className="w-full p-2 border border-gray-700 rounded-md"
+                    />
+                    <textarea
+                        name="contractAbi"
+                        id=""
+                        placeholder="Enter contract ABI"
+                        className="w-full h-24 p-2 border border-gray-700 rounded-md text-gray"
+                    />
+                    <button>Generate UI</button>
+                </div>
+            </div>
         </div>
     );
 }
